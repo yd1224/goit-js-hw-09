941ddba7f54b710575daa5dcadf2283216e1600d
Yelyzaveta1224 <demcenkoelizaveta6@gmail.com>
2023-11-29 18:34:23 +0100

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,23 @@
+const btn = document.querySelector('[type="submit"]')
+const form = document.querySelector(".form")
+
+
+
+
+form.addEventListener("submit", handleInput)
+function handleInput(event) {
+    event.preventDefault(); 
+  const delayValue = form.delay.value;
+  const stepValue = form.step.value;
+  const amountValue = form.amount.value;
+  console.log(delayValue, stepValue, amountValue);
+
+  setInterval((delayValue,stepValue ) => {
+    createPromise(amountValue, stepValue)
+  },amountValue)
+}
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
