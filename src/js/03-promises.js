b3784d6023a278fbb13bbe8ff069c148744b9149
Yelyzaveta1224 <demcenkoelizaveta6@gmail.com>
2023-11-30 17:17:54 +0100

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,28 +1,73 @@
-const btn = document.querySelector('[type="submit"]')
+
+import Notiflix from 'notiflix';
 const form = document.querySelector(".form")
 
 
 
 
-form.addEventListener("submit", handleInput)
-function handleInput(event) {
+form.addEventListener("submit", handleSubmit)
+function handleSubmit(event) {
     event.preventDefault(); 
-  const delayValue = form.delay.value;
-  const stepValue = form.step.value;
-  const amountValue = form.amount.value;
-  console.log(delayValue, stepValue, amountValue);
+  const delayValue = +form.delay.value;
+  const stepValue = +form.step.value;
+  const amountValue = +form.amount.value;
+  for (let i = 1, delay =delayValue; i <= amountValue; i += 1, delay += stepValue){
+    setTimeout(() => {
+      createPromise(i, delay)
+    .then(({ position, delay }) => {
+      // console.log(` Fulfilled promise ${position} in ${delay}ms`);
+      Notiflix.Notify.success(
+  ` Fulfilled promise ${position} in ${delay}ms`,
 
-  setInterval((delayValue,stepValue ) => {
-    createPromise(amountValue, stepValue)
-  },amountValue)
-}
+);
+  })
+        .catch(({ position, delay }) => {
+     Notiflix.Notify.failure(
+  ` Rejected promise ${position} in ${delay}ms`,
+ 
+);
+    // console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+},delay)
 
 
-function createPromise(position, delay) {
+  }
+  function createPromise(position, step) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  
+    return Promise.resolve({
+      position: position,
+      delay: step
+
+    });
   }
+  else {
+  
+          return Promise.reject({
+            position: position,
+            delay: step
+          });
+
+    }
+    }
+
+
 }
+
+
+
+
+
+//   if (selectedDates[0].getTime() - options.defaultDate.getTime() < 0) {
+// Notiflix.Notify.failure(
+//   'Please choose a date in the future',
+//   {
+//     timeout: 2000,
+//   },
+// );
+//  startBtn.disabled = true;
+// } else {
+//           startBtn.disabled = false;
+// }
+
