de01c0fe7b8435d07073366e41225c5688973a5e
yd1224 <138761874+yd1224@users.noreply.github.com>
2023-11-30 16:18:19 +0000

    Deploying to gh-pages from @ yd1224/goit-js-hw-09@b3784d6023a278fbb13bbe8ff069c148744b9149 🚀

--- 03-promises.2e1f8375.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelector('[type="submit"]');var e=document.querySelector(".form");e.addEventListener("submit",(function(t){t.preventDefault();var n=e.delay.value,u=e.step.value,o=e.amount.value;console.log(n,u,o),setInterval((function(e,t){Math.random()}),o)}))}();
-//# sourceMappingURL=03-promises.2e1f8375.js.map
--- 03-promises.f50a1265.js
+++
@@ -1,2 +0,0 @@
-document.querySelector('[type="submit"]');const e=document.querySelector(".form");e.addEventListener("submit",(function(t){t.preventDefault();const n=e.delay.value,o=e.step.value,u=e.amount.value;console.log(n,o,u),setInterval(((e,t)=>{Math.random()}),u)}));
-//# sourceMappingURL=03-promises.f50a1265.js.map
